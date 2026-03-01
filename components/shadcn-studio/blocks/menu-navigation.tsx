23aab50c66a7d62423601868f3939ad40b6016b5
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2026-03-01 22:46:04 +0530

    fix: remove unnecessary viewport prop from NavigationMenu

--- components/shadcn-studio/blocks/menu-navigation.tsx
+++ components/shadcn-studio/blocks/menu-navigation.tsx
@@ -38,7 +38,7 @@ type MenuNavigationProps = {
 
 const MenuNavigation = ({ navigationData, className }: MenuNavigationProps) => {
   return (
-    <NavigationMenu viewport={false} className={className}>
+    <NavigationMenu className={className}>
       <NavigationMenuList className='flex-wrap justify-start gap-0'>
         {navigationData.map(navItem => {
           if (navItem.href) {
