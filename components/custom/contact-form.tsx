4d73f49336a3defa06ab1e948de65d34b5b34eae
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2026-05-15 14:33:34 +0530

    feat: product image update

--- components/custom/contact-form.tsx
+++ components/custom/contact-form.tsx
@@ -90,7 +90,7 @@ export function ContactForm() {
         {isPending ? (
           <>
             <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            Sending...
+            Submitting...
           </>
         ) : (
           "Send Message"
