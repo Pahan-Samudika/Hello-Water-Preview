6b5027bfeb5f7b836c57f9c736d3a8734530890e
Pahan Abhayawardhane <pahanabhayawardhane@gmail.com>
2026-06-11 14:54:03 +0530

    feat: convert sitemap generation to async fetching from Firestore

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,5 @@
 import type { MetadataRoute } from "next";
-
-import { products } from "@/constants/products";
-import { blogs } from "@/constants/blogs";
+import { getProducts, getBlogs } from "@/lib/db-queries";
 import { absoluteUrl } from "@/lib/seo";
 
 export const revalidate = 86400;
@@ -19,8 +17,13 @@ const staticRoutes = [
   "/blogs",
 ] as const;
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const lastModified = new Date();
+
+  const [products, blogs] = await Promise.all([
+    getProducts(),
+    getBlogs(),
+  ]);
 
   return [
     ...staticRoutes.map((route) => ({
